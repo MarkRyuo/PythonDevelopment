1face4da09ec23059517333fadc698e64add48d9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-11 17:36:07 +0800

    update aug 11 2023

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # imports 
 from flask import FLask  # Import Flask 
 from flask_scss import Scss  # importing Flask Sass
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy # Import flask-sqlalchemy 
+
+
 
