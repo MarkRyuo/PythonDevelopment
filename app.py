0b6a8759aaf36382370c35db3c3d7ef4ee26f0be
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-11 17:38:27 +0800

    update aug 11 2023

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 # imports 
-from flask import FLask  # Import Flask 
+from flask import Flask  # Import Flask 
 from flask_scss import Scss  # importing Flask Sass
 from flask_sqlalchemy import SQLAlchemy # Import flask-sqlalchemy 
 
+# My app 
+app = Flask(__name__) 
 
+
+def index() :
+    return "testing 123"
 
