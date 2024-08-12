d3fd85cbd3d25659c354d5ed85d50a9543c83c1b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-12 20:15:23 +0800

    update aug 12 2004

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ app = Flask(__name__)
 def index() :
     return render_template("index.html")
 
+@app.route(/)
+def homepage():
+    return render_template("Homepage.html")
+
 if __name__ == '__main__' :
     app.run(debug=True)
