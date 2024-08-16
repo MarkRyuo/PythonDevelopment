72b70b015d2785ac91e8fdafd3abe9161ef50067
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-16 17:23:43 +0800

    Update: Aug 16 2024

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # imports 
 from flask import Flask, render_template  # Import Flask 
-# from flask_scss import Scss  # importing Flask Sass
+from flask_scss import Scss  # importing Flask Sass
 # from flask_sqlalchemy import SQLAlchemy # Import flask-sqlalchemy 
 
 # My app 
 app = Flask(__name__) 
-
+Scss(app)
 
 @app.route("/") # always add / for route 
 def index() :
